42db11e24e25f6e9253e3e81d6e6fa4b89cb1c9e
nitin122 <loanedzservices@gmail.com>
2026-03-20 00:29:58 +0530

    Revert "Initial commit"
    This reverts commit 4be12f67738fc16778a927d318750660de0d3610.

--- start-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting BABISHA Virtual Try-On Backend Server...
-echo.
-node server.js
-pause
-
-
--- start-server.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Starting BABISHA Virtual Try-On Backend Server..." -ForegroundColor Green
-Write-Host ""
-node server.js
-
-
